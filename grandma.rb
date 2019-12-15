7a86bfba0fde2466ea55a4b57c1499119bc30526
Brigette LeVert <bnl.levert@gmail.com>
2019-12-15 21:22:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,9 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  end
-  if string != string.upcase
+  elseif string == string.upcase
+    return "NO, NOT SINCE 1938!"
+    elseif string != string.upcase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end   
